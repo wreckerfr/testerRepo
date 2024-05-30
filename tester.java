712fb2e27cdb802fd33484f381b3e698bc0616b0
wreck <wreck@wreck>
2024-05-30 21:56:00 +0530

    This is the second commit

---
+++ tester.java
@@ -0,0 +1,10 @@
+package gitTester.testerRepo;
+
+public class tester {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is the tester java file for checking oput the -am flag");
+
+    }
+}
